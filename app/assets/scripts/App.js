cafb72ce60cb464c04f062fede279dd15b42e0ea
Steve Gallant <sgallant4@gmail.com>
2020-08-07 13:00:22 -0400

    Render quote box using React

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,34 @@
 import '../styles/styles.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import quotations from './modules/_quotations.js';
 
+class QuoteBox extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  clickHandler() {
 
+  }
+
+  render() {
+    return (
+      <div id="quote-box" className="quote-box">
+        <div id="text" className="quote-box__quoteText">
+          <p>To be or not to be...</p>
+        </div>
+        <div id="author" className="quote-box__author">
+          - some mad dane
+        </div>
+        <a href="#" id="tweet-quote" className="btn btn__twitter" title="Tweet this quote"><i className="fa fa-twitter" aria-hidden="true"></i></a>
+        <a id="new-quote" className="btn btn__next-quote" href="#">Next Quote</a>
+
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<QuoteBox />, document.querySelector("#app"));
 
 if (module.hot) {
   module.hot.accept();
---
+++ app/assets/scripts/modules/_quotations.js
@@ -0,0 +1,14 @@
+// Static array of quotation examples
+
+const quotations = [
+  {
+    author: "Homer Simpson",
+    content: "Doh!"
+  },
+  {
+    author: "John MacLane",
+    content: "Yippee-ki-yay, motherfucker!"
+  }
+];
+
+export default quotations;
