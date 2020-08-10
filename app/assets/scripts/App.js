bf7aa9d7d149e9728a7e6403c445b8b76657f1cb
Steve Gallant <sgallant4@gmail.com>
2020-08-10 16:15:59 -0400

    Complete twitter button. Style background image

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,27 +1,42 @@
 import '../styles/styles.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import quotations from './modules/_quotations.js';
+import quotations from './modules/_quotations';
+
+let tweetURL;
 
 class QuoteBox extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      quoteText: "To be or not to be...",
+      attribution: "some mad Dane"
+    };
+    this.handleClick = this.handleClick.bind(this);
   }
-  clickHandler() {
-
+  handleClick(e) {
+    e.preventDefault();
+    let rand = Math.floor(Math.random() * quotations.length);
+    let randomQuote = quotations[rand];
+    this.setState({
+      quoteText: randomQuote.content,
+      attribution: randomQuote.attribution
+    });
+    // tweetURL = 'https://twitter.com/intent/tweet?text=%22' + this.state.quoteText + '%22%20%20-%20' + this.state.attribution;
+    tweetURL = "https://twitter.com/intent/tweet?text=hello";
   }
 
   render() {
     return (
       <div id="quote-box" className="quote-box">
         <div id="text" className="quote-box__quoteText">
-          <p>To be or not to be...</p>
+          <p>&ldquo; {this.state.quoteText} &rdquo;</p>
         </div>
         <div id="author" className="quote-box__author">
-          - some mad dane
+          {'- ' + this.state.attribution}
         </div>
-        <a href="#" id="tweet-quote" className="btn btn__twitter" title="Tweet this quote"><i className="fa fa-twitter" aria-hidden="true"></i></a>
-        <a id="new-quote" className="btn btn__next-quote" href="#">Next Quote</a>
+        <a href={"https://twitter.com/intent/tweet?text=%22" + this.state.quoteText + "%22%20--%20" + this.state.attribution} id="tweet-quote" className="btn btn__twitter" title="Tweet this quote" target="_blank"><i className="fa fa-twitter" aria-hidden="true"></i></a>
+        <a id="new-quote" className="btn btn__next-quote" href="#" onClick={this.handleClick}>Next Quote</a>
 
       </div>
     );
--- app/assets/scripts/modules/_quotations.js
+++ app/assets/scripts/modules/_quotations.js
@@ -1,14 +1,16 @@
 // Static array of quotation examples
 
 const quotations = [
-  {
-    author: "Homer Simpson",
-    content: "Doh!"
-  },
-  {
-    author: "John MacLane",
-    content: "Yippee-ki-yay, motherfucker!"
-  }
+  {attribution: "Homer Simpson", content: "Doh!"},
+  {attribution: "John MacLane", content: "Yippee-ki-yay, motherfucker!"},
+  {attribution: "Bruce Lee", content: "The successful warrior is the average man, with laser-like focus."},
+  {attribution: "Steve Jobs", content: "Stay hungry, stay foolish."},
+  {attribution: "Malcolm X", content: "The future belongs to those who prepare for it today."},
+  {attribution: "J.R.R. Tolkien", content: "Little by little, one travels far."},
+  {attribution: "Marcus Aurelius", content: "You have power over your mind – not outside events. Realize this, and you will find strength."},
+  {attribution: "Wayne Gretzky", content: "You miss 100 percent of the shots you never take."},
+  {attribution: "Henry Ford", content: "If you think you can do a thing or think you can’t do a thing, you’re right."},
+  {attribution: "Friedrich Nietzsche", content: "That which does not kill us makes us stronger."},
 ];
 
 export default quotations;
